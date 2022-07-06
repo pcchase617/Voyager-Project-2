f412dc4c36d103f1a536eb98762ccd7bfa1069f1
Camilo Arango <camilo94@icloud.com>
2022-07-05 21:30:32 -0700

    app is working now

--- public/js/frontend.js
+++
@@ -1,4 +0,0 @@
-// example frontend js file loaded into the views that need FE logic (clicks, forms, interactions, etc.)
-console.log('frontend.js loaded');
-
-//do other frontend stuff here!
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 const app = express();
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(require('./controllers/homeRoutes'));
 console.log(process.env.API_KEY);
 

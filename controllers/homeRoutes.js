44230c9001688e50266c4629d3613bec3f9a008f
Luna Kiira <lunakiira0911@gmail.com>
2022-07-09 08:38:01 -0700

    accept chances to two files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,11 +5,7 @@ const withAuth = require('../utils/helpers');
 
 //when a GET request is received on the root(/) route,
 //render the home.handlebars view
-<<<<<<< HEAD
-router.get('/', withAuth, (req, res) => {
-=======
 router.get('/booking', (req, res) => {
->>>>>>> fe4dea6433d14b03996b6a650e54fd95f5d0b2f6
   res.render('home');
 });
 
@@ -22,12 +18,9 @@ router.get('/login', (req, res) => {
   //this will render the login
   res.render('login');
 });
-<<<<<<< HEAD
-=======
 
 router.get('/', (req, res) => {
   res.render('homepage');
 });
->>>>>>> fe4dea6433d14b03996b6a650e54fd95f5d0b2f6
 
 module.exports = router;
